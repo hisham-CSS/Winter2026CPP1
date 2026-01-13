7c482b0c1a2d0d6ec40a234dba7fc0473c38e405
Hisham <hisham@catsplatstudios.com>
2026-01-13 11:34:56 -0500

    Lab 2 incomplete

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //input handling
+        float horizontalInput = Input.GetAxis("Horizontal");
+        bool jumpInput = Input.GetButtonDown("Jump");
+
+        //movement
+        Vector2 velocity = rb.linearVelocity;
+        velocity.x = horizontalInput * moveSpeed;
+        rb.linearVelocity = velocity;
+
+        //jumping
+        if (jumpInput)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
