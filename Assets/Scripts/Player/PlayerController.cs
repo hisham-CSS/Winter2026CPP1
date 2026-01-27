9d931711653d64b9efecc4f414b28dfdeec5475c
Hisham <hisham@catsplatstudios.com>
2026-01-27 11:02:37 -0500

    Lab 4 incomplete

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
     private GroundCheck _groundCheck;
     
     private bool _isGrounded = false;
+    private bool _isFiring = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -41,11 +42,15 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
         bool jumpInput = Input.GetButtonDown("Jump");
+        bool fireInput = Input.GetButtonDown("Fire1");
 
         //movement
-        Vector2 velocity = _rb.linearVelocity;
-        velocity.x = horizontalInput * moveSpeed;
-        _rb.linearVelocity = velocity;
+        if (!_isFiring)
+        {
+            Vector2 velocity = _rb.linearVelocity;
+            velocity.x = horizontalInput * moveSpeed;
+            _rb.linearVelocity = velocity;
+        }
 
         if (horizontalInput != 0) SpriteFlip(horizontalInput);
         
@@ -54,11 +59,18 @@ public class PlayerController : MonoBehaviour
         {
             _rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
         }
+        //shooting
+        if (fireInput && !_isFiring)
+        {
+            //_rb.linearVelocity = Vector2.zero;
+            _isFiring = true;
+        }
 
         //animation
         _anim.SetFloat("moveInput", Mathf.Abs(horizontalInput));
         _anim.SetFloat("yVel", _rb.linearVelocity.y);
         _anim.SetBool("isGrounded", _isGrounded);
+        _anim.SetBool("Fire", _isFiring);
     }
 
     /// <summary>
@@ -66,4 +78,12 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     /// <param name="horizontalInput">The input received from Unity's input system</param>
     private void SpriteFlip(float horizontalInput) => _sr.flipX = (horizontalInput < 0);
+
+    /// <summary>
+    /// Animation event function to reset the firing animation state
+    /// </summary>
+    public void ResetFireAnimation()
+    {
+        _isFiring = false;
+    }
 }
