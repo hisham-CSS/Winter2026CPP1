7ea0aacfcb27776f2b93f0dcf7c1a549e54f52be
Hisham <hisham@catsplatstudios.com>
2026-03-03 09:48:51 -0500

    Lab 8 midpoint

--- Assets/Scripts/Enemy/TurretEnemy.cs
+++ Assets/Scripts/Enemy/TurretEnemy.cs
@@ -6,7 +6,10 @@ public class TurretEnemy : BaseEnemy
     Shoot shoot;
 
     [SerializeField] private float fireRate = 2f; // Shots per second
+    [SerializeField] private float distanceThreshold = 5f; // Distance at which the turret will start firing
     private float timeSinceLastFire = 0f;
+
+    private PlayerController playerRef;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public override void Start()
@@ -22,6 +25,8 @@ public class TurretEnemy : BaseEnemy
         }
 
         shoot.OnProjectileFired += () => timeSinceLastFire = Time.time;
+        //shoot.OnProjectileFired += OnProjectileFiredCallback;
+        GameManager.Instance.OnPlayerSpawned += (PlayerController player) => playerRef = player;
     }
 
     //we could add the callback to the shoot script and subscribe to it in the turret enemy script - this ensures that the timing is correct for the time since last fired - however since this is one line of code - we can make a lambda function when we subsribe to the event instead of creating a separate function for it. This is a more concise way to achieve the same result without cluttering the code with an additional method that is only used once.
@@ -33,6 +38,17 @@ public class TurretEnemy : BaseEnemy
     // Update is called once per frame
     void Update()
     {
+        if (playerRef == null) return;
+
+        if (!CheckDistance())
+        {
+            sr.color = Color.white;
+            return;
+        }
+
+        sr.flipX = playerRef.transform.position.x < transform.position.x;
+        sr.color = Color.red;
+
         AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
 
         if (stateInfo.IsName("Idle"))
@@ -42,5 +58,11 @@ public class TurretEnemy : BaseEnemy
                 anim.SetTrigger("Fire");
             }
         }
+    }
+
+    bool CheckDistance()
+    {
+        float distanceToPlayer = Mathf.Abs(transform.position.x - playerRef.transform.position.x);
+        return distanceToPlayer <= distanceThreshold;
     }
 }
